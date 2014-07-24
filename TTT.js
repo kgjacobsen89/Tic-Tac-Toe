4399dfe4766994161c6afb6573e03d273e4519a8
kgjacobsen89 <kgjacobsen89@gmail.com>
2014-07-23 20:22:16 -0700

    commit before attempting firebase

--- TTT.js
+++ TTT.js
@@ -6,21 +6,38 @@ TicTacToeapp.controller("TicTacToecontroller",function($scope){
 	[{owner:""},{owner:""},{owner:""}],
 	[{owner:""},{owner:""},{owner:""}]
 	];
+	
 
-	$scope.Player_Name = "Player 1";
+	$scope.formOne = false;
+	$scope.formTwo = false;
+
+	$scope.submit = function(player) {
+		if (player==1) {
+			$scope.formOne = true;
+		}
+		else if (player==2) {
+			$scope.formTwo = true;
+		}
+		$scope.Player_Name = $scope.textOne;
+	};
+
+	
+	$scope.Player_Turn = "Player 1";
 
 	$scope.claim = function(box){
 		if (box.owner == "P1" || box.owner == "P2") {
-			alert("Sorry, that's already taken!");
-		} else if ($scope.Player_Name == "Player 1") {				
+	 		alert("Sorry, that's already taken!");
+		} else if ($scope.Player_Turn == "Player 1") {
 			box.owner = "P1";
-			$scope.Player_Name = "Player 2";
+			$scope.Player_Turn = "Player 2";
+			$scope.Player_Name = $scope.textTwo;
 			$scope.compare();
-		} else if ($scope.Player_Name == "Player 2") {				
-				box.owner = "P2";
-				$scope.Player_Name = "Player 1";
-				$scope.compare();
-		} 
+		} else if ($scope.Player_Turn == "Player 2") {				
+			box.owner = "P2";
+			$scope.Player_Turn = "Player 1";
+			$scope.Player_Name = $scope.textOne;
+			$scope.compare();
+		}
 	};
 
 
@@ -36,70 +53,87 @@ TicTacToeapp.controller("TicTacToecontroller",function($scope){
 		var box9 = $scope.boxes[2][2];	
 
 		if (box1.owner == "P1" && box2.owner == "P1" && box3.owner == "P1"){
-			alert("P1 wins!");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box4.owner == "P1" && box5.owner == "P1" && box6.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box7.owner == "P1" && box8.owner == "P1" && box9.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box1.owner == "P1" && box4.owner == "P1" && box7.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box2.owner == "P1" && box5.owner == "P1" && box8.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box3.owner == "P1" && box6.owner == "P1" && box9.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne  + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box1.owner == "P1" && box5.owner == "P1" && box9.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box3.owner == "P1" && box5.owner == "P1" && box7.owner == "P1"){
-			alert("P1 wins");
+			alert($scope.textOne + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box1.owner == "P2" && box2.owner == "P2" && box3.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box4.owner == "P2" && box5.owner == "P2" && box6.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box7.owner == "P2" && box8.owner == "P2" && box9.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box1.owner == "P2" && box4.owner == "P2" && box7.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box2.owner == "P2" && box5.owner == "P2" && box8.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + "wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box3.owner == "P2" && box6.owner == "P2" && box9.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box1.owner == "P2" && box5.owner == "P2" && box9.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if (box3.owner == "P2" && box5.owner == "P2" && box7.owner == "P2"){
-			alert("P2 wins");
+			alert($scope.textTwo + " wins!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		} else if ((box1.owner == "P1" || box1.owner == "P2") && (box2.owner == "P1" || box2.owner == "P2") && (box3.owner == "P1" || box3.owner == "P2") && (box4.owner == "P1" || box4.owner == "P2") && (box5.owner == "P1" || box5.owner == "P2") && (box6.owner == "P1" || box6.owner == "P2") && (box7.owner == "P1" || box7.owner == "P2") && (box8.owner == "P1" || box8.owner == "P2") && (box9.owner == "P1" || box9.owner == "P2")){
-			alert("Catsgame!!!");
+			alert("Cat's Game!!!");
 			replay(box1,box2,box3,box4,box5,box6,box7,box8,box9);
 		}
 	};
 
-	function replay(b1,b2,b3,b4,b5,b6,b7,b8,b9){
-		$scope.Player_Name = "Player 1";
-		b1.owner ="";
-		b2.owner ="";
-		b3.owner ="";
-		b4.owner ="";
-		b5.owner ="";
-		b6.owner ="";
-		b7.owner ="";
-		b8.owner ="";
-		b9.owner ="";
+	function replay(box1,box2,box3,box4,box5,box6,box7,box8,box9){
+
+		$scope.formOne = false;
+		$scope.formTwo = false;
+
+		$scope.submit = function(player) {
+			if (player==1) {
+				$scope.formOne = true;
+			}
+			else if (player==2) {
+				$scope.formTwo = true;
+			}
+			$scope.Player_Name = $scope.textOne;
+		};
+
+		$scope.Player_Turn = "Player 1";
+		$scope.Player_Name = $scope.textOne;
+
+		box1.owner ="";
+		box2.owner ="";
+		box3.owner ="";
+		box4.owner ="";
+		box5.owner ="";
+		box6.owner ="";
+		box7.owner ="";
+		box8.owner ="";
+		box9.owner ="";
+
 	};
 
 
